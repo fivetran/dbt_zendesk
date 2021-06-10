9a007d6b980dfa443ea60ddeab2708d6827837b5
matthew-skinner <82058907+matthew-skinner@users.noreply.github.com>
2021-06-10 08:22:08 -0500

    Update models/zendesk__ticket_metrics.sql
    Co-authored-by: Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>

--- models/zendesk__ticket_metrics.sql
+++ models/zendesk__ticket_metrics.sql
@@ -95,7 +95,7 @@ select
     then({{ dbt_utils.datediff("ticket_enriched.assignee_last_login_at", dbt_utils.current_timestamp(), 'second') }} /60)
       end as assignee_last_login_age_minutes,
   case when lower(ticket_enriched.status) not in ('solved','closed')
-    then round(({{ dbt_utils.datediff("ticket_enriched.created_at", dbt_utils.current_timestamp(), 'second') }} /60),2)
+    then({{ dbt_utils.datediff("ticket_enriched.created_at", dbt_utils.current_timestamp(), 'second') }} /60)
       end as unsolved_ticket_age_minutes,
   case when lower(ticket_enriched.status) not in ('solved','closed')
     then round(({{ dbt_utils.datediff("ticket_enriched.updated_at", dbt_utils.current_timestamp(), 'second') }} /60),2)
