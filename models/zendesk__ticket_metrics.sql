669983abe1d8c77c822b3523f774e834c2f942ba
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2021-06-14 17:19:11 -0500

    Update models/zendesk__ticket_metrics.sql
    Co-authored-by: Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>

--- models/zendesk__ticket_metrics.sql
+++ models/zendesk__ticket_metrics.sql
@@ -93,7 +93,7 @@ select
     then ({{ dbt_utils.datediff("ticket_enriched.requester_last_login_at", dbt_utils.current_timestamp(), 'second') }} /60)
       end as requester_last_login_age_minutes,
   case when ticket_enriched.is_assignee_active = true and ticket_enriched.assignee_last_login_at is not null
-    then({{ dbt_utils.datediff("ticket_enriched.assignee_last_login_at", dbt_utils.current_timestamp(), 'second') }} /60)
+    then ({{ dbt_utils.datediff("ticket_enriched.assignee_last_login_at", dbt_utils.current_timestamp(), 'second') }} /60)
       end as assignee_last_login_age_minutes,
   case when lower(ticket_enriched.status) not in ('solved','closed')
     then({{ dbt_utils.datediff("ticket_enriched.created_at", dbt_utils.current_timestamp(), 'second') }} /60)
