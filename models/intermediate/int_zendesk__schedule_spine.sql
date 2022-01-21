a5b1f87bb3b158e210b128069756239f937edd94
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2022-01-21 14:02:07 -0800

    remove commenting

--- models/intermediate/int_zendesk__schedule_spine.sql
+++ models/intermediate/int_zendesk__schedule_spine.sql
@@ -5,43 +5,10 @@ with timezone as (
     select *
     from {{ var('time_zone') }}
 
-{# ), year_spine as (
-
-    {{ dbt_utils.date_spine(
-        datepart = "year", 
-        start_date = dbt_utils.dateadd("year", - var('ticket_field_history_timeframe_years'), dbt_utils.date_trunc('year', dbt_utils.current_timestamp())),
-        end_date = dbt_utils.dateadd("year", 1, dbt_utils.date_trunc('year', dbt_utils.current_timestamp())) 
-        ) 
-    }} 
-
-), timezone_year_spine as (
-
-    select 
-        timezone.time_zone,
-        timezone.standard_offset_minutes,
-        year_spine.date_year,
-        extract(year from year_spine.date_year) as year
-
-    from timezone 
-    join year_spine on true #}
-
 ), daylight_time as (
 
     select *
     from {{ var('daylight_time') }}
-{# 
-), timezone_dt as (
-
-    select
-        timezone_year_spine.*,
-        daylight_time.daylight_start_utc,
-        daylight_time.daylight_end_utc,
-        daylight_time.daylight_offset_minutes
-
-    from timezone_year_spine
-    left join daylight_time 
-        on timezone_year_spine.time_zone = daylight_time.time_zone
-        and timezone_year_spine.year = daylight_time.year #}
 
 ), schedule as (
 
