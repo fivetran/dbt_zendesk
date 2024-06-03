5f2113be9fd75c924a6afaefa4fc5e73abc35112
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2024-06-03 15:09:28 -0700

    first try

--- macros/regex_extract.sql
+++ macros/regex_extract.sql
@@ -15,3 +15,27 @@ REGEXP_EXTRACT({{ string }}, {%- if start_or_end == 'start' %} r'{"([^"]+)"' {% 
 REGEXP_EXTRACT({{ string }}, {%- if start_or_end == 'start' %} r'{"([^"]+)"' {% else %} r'":"([^"]+)"}' {% endif -%} )
 
 {% endmacro %}
+
+{% macro snowflake__regex_extract(string, start_or_end) %}
+
+REGEXP_SUBSTR({{ string }}, {%- if start_or_end == 'start' %} '"([^"]+)"' {% else %} '": "([^"]+)"' {% endif -%}, 1, 1, 'e', 1 )
+
+{% endmacro %}
+
+{% macro postgres__regex_extract(string, start_or_end) %}
+
+(regexp_matches({{ string }}, {%- if start_or_end == 'start' %} '"([^"]+)":' {% else %} '": "([^"]+)' {% endif -%} ))[1]
+
+{% endmacro %}
+
+{% macro redshift__regex_extract(string, start_or_end) %}
+
+REGEXP_EXTRACT({{ string }}, {%- if start_or_end == 'start' %} '"([^"]+)"' {% else %} '": "([^"]+)"' {% endif -%}, 1, 1, 'e')
+
+{% endmacro %}
+
+{% macro spark__regex_extract(string, start_or_end) %}
+
+regexp_extract({{ string }}, {%- if start_or_end == 'start' %} '"([^"]+)":' {% else %} '": "([^"]+)"' {% endif -%}, 1)
+
+{% endmacro %}
