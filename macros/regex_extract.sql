505f3431d83786c6eb1c4f0fb0c1ac45c2e02e53
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-09-23 15:41:14 -0500

    update schedule_history

--- macros/regex_extract.sql
+++ macros/regex_extract.sql
@@ -8,34 +8,35 @@
 
     regexp_extract({{ string }}, {{ regex }} )
 
-{% endmacro %}
+{%- endmacro %}
 
 {% macro bigquery__regex_extract(string, regex) %}
 
     regexp_extract({{ string }}, {{ regex }} )
 
-{% endmacro %}
+{%- endmacro %}
 
 {% macro snowflake__regex_extract(string, regex) %}
 
     REGEXP_SUBSTR({{ string }}, {{ regex }}, 1, 1, 'e', 1 )
 
-{% endmacro %}
+{%- endmacro %}
 
 {% macro postgres__regex_extract(string, regex) %}
 
     (regexp_matches({{ string }}, {{ regex }}))[1]
 
-{% endmacro %}
+{%- endmacro %}
 
 {% macro redshift__regex_extract(string, regex) %}
 
-    REGEXP_SUBSTR({{ string }}, {{ regex }}, 1, 1, 'e')
+    {% set reformatted_regex = regex | replace(".*?", ".*") | replace("{", "\\\{") | replace("}", "\\\}") -%}
+    REGEXP_SUBSTR({{ string }}, {{ reformatted_regex }}, 1, 1, 'e')
 
-{% endmacro %}
+{%- endmacro %}
 
 {% macro spark__regex_extract(string, regex) %}
+    {% set reformatted_regex = regex | replace("{", "\\\{") | replace("}", "\\\}") -%}
+    regexp_extract({{ string }}, {{ reformatted_regex }}, 1)
 
-    regexp_extract({{ string }}, {{ regex }}, 1)
-
-{% endmacro %}
+{%- endmacro %}
--- models/intermediate/int_zendesk__schedule_history.sql
+++ models/intermediate/int_zendesk__schedule_history.sql
@@ -13,11 +13,11 @@ with audit_logs as (
         schedule_id,
         created_at,
         min(created_at) over (partition by schedule_id) as min_created_at,
-        replace(replace(replace(replace(change_description, 
-            '&quot;', '"') , 
-            'amp;', '') , 
-            '=&gt;', ':'), 
-            ' ', '')
+        replace(replace(replace(replace(change_description,
+            'workweek changed from', ''), 
+            '&quot;', '"'), 
+            'amp;', ''), 
+            '=&gt;', ':')
             as change_description_cleaned
     from audit_logs
 
@@ -27,7 +27,7 @@ with audit_logs as (
         audit_logs_enhanced.*,
         cast('1970-01-01' as {{ dbt.type_timestamp() }}) as valid_from,
         created_at as valid_to,
-        {{ dbt.split_part('change_description_cleaned', "'to'", 1) }} as schedule_change,
+        {{ dbt.split_part('change_description_cleaned', "' to '", 1) }} as schedule_change,
         'from' as change_type -- remove before release but helpful for debugging
     from audit_logs_enhanced
     where created_at = min_created_at -- the 'from' portion only matters for the first row
@@ -43,66 +43,59 @@ with audit_logs as (
                 partition by schedule_id order by created_at), 
             {{ dbt.current_timestamp_backcompat() }})
             as valid_to,
-        {{ dbt.split_part('change_description_cleaned', "'to'", 2) }} as schedule_change,
+        {{ dbt.split_part('change_description_cleaned', "' to '", 2) }} as schedule_change,
         'to' as change_type -- remove before release but helpful for debugging
     from audit_logs_enhanced
 
 ), split_days as (
     {% set days_of_week = {'sun': 0, 'mon': 1, 'tue': 2, 'wed': 3, 'thu': 4, 'fri': 5, 'sat': 6} %}
     {% for day, day_number in days_of_week.items() %}
-        select
-            split_to_from.*,
-            '{{ day }}' as day_of_week,
-            cast('{{ day_number }}' as {{ dbt.type_int() }}) as day_of_week_number,
-            replace(
-                {{ dbt.concat([
-                    '"["',
-                    zendesk.regex_extract('schedule_change', "'.*?" ~ day ~ ".*?({.*?})'"),
-                    '"]"']) }}
-                , ',', '},{')
-                as schedule_change_cleaned
-        from split_to_from
+    select
+        split_to_from.*,
+        '{{ day }}' as day_of_week,
+        cast('{{ day_number }}' as {{ dbt.type_int() }}) as day_of_week_number,
+        {{ zendesk.regex_extract('schedule_change', "'.*?" ~ day ~ ".*?({.*?})'") }} as day_of_week_schedule
+    from split_to_from
     {% if not loop.last %}union all{% endif %}
     {% endfor %}
 
 ), unnested_schedules as (
-    -- only want the first "from_schedule" to start off
-    select 
-        split_days.*,
-        replace(replace(replace(unnested_schedule, '{', ''), '}', ''), '"', '') as cleaned_unnested_schedule
-    from split_days
-    -- need to update for all warehouses
-    cross join {{ zendesk.unnest_json_array('schedule_change_cleaned') }} as unnested_schedule
-
-), split_times as (
-
-    select 
-        schedule_id,
-        cast(nullif({{ dbt.split_part('cleaned_unnested_schedule', "':'", 1) }}, ' ') as {{ dbt.type_int() }}) as start_time_hh, 
-        cast(nullif({{ dbt.split_part('cleaned_unnested_schedule', "':'", 2) }}, ' ') as {{ dbt.type_int() }}) as start_time_mm, 
-        cast(nullif({{ dbt.split_part('cleaned_unnested_schedule', "':'", 3) }}, ' ') as {{ dbt.type_int() }}) as end_time_hh, 
-        cast(nullif({{ dbt.split_part('cleaned_unnested_schedule', "':'", 4) }}, ' ') as {{ dbt.type_int() }}) as end_time_mm, 
-        day_of_week,
-        day_of_week_number,
-        _fivetran_synced,
-        valid_from,
-        valid_to
-    from unnested_schedules
-),
-
-final as (
-
     select
-        _fivetran_synced,
-        schedule_id,
-        valid_from,
-        valid_to,
-        start_time_hh * 60 + start_time_mm + day_of_week_number * 24 * 60 as start_time,
-        end_time_hh * 60 + end_time_mm + day_of_week_number * 24 * 60 as end_time,
-        day_of_week,
-        day_of_week_number
-    from split_times
+        split_days.*,
+
+{%- if target.type == 'bigquery' %}
+        replace(replace(replace(replace(unnested_schedule, '{', ''), '}', ''), '"', ''), ' ', '') as cleaned_unnested_schedule
+    from split_days
+    cross join unnest(json_extract_array('[' || replace(day_of_week_schedule, ',', '},{') || ']', '$')) as unnested_schedule
+
+{%- elif target.type == 'snowflake' %}
+        unnested_schedule.key || ':' || unnested_schedule.value as cleaned_unnested_schedule
+    from split_days
+    cross join lateral flatten(input => parse_json(replace(replace(day_of_week_schedule, '\}\}', '\}'), '\{\{', '\{'))) as unnested_schedule
+
+{%- elif target.type == 'postgres' %}
+        replace(replace(replace(replace(unnested_schedule::text, '{', ''), '}', ''), '"', ''), ' ', '') as cleaned_unnested_schedule
+    from split_days
+    cross join lateral jsonb_array_elements(('[' || replace(day_of_week_schedule, ',', '},{') || ']')::jsonb) as unnested_schedule
+
+{%- elif target.type in ('databricks', 'spark') %}
+        replace(replace(replace(replace(unnested_schedule, '{', ''), '}', ''), '"', ''), ' ', '') as cleaned_unnested_schedule
+    from split_days
+    lateral view explode(from_json(concat('[', replace(day_of_week_schedule, ',', '},{'), ']'), 'array<string>')) as unnested_schedule
+
+{%- elif target.type == 'redshift' %}
+    {# json_parse('[' || replace(replace(day_of_week_schedule, '\}\}', '\}'), '\{\{', '\{') || ']') as json_schedule
+    from split_days #}
+    {# cross join lateral json_parse(replace(replace(day_of_week_schedule, '\}\}', '\}'), '\{\{', '\{')) as element #}
+
+        cast(null as {{ dbt.type_string() }}) as cleaned_unnested_schedule
+    from split_days
+
+{% else %}
+        cast(null as {{ dbt.type_string() }}) as cleaned_unnested_schedule
+    from split_days
+{%- endif %}
 )
 
 select * 
-from final
+from unnested_schedules
