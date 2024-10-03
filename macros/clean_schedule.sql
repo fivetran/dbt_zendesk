c117df971ca103fdf66471ca6f691e67710a1006
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2024-10-03 16:04:29 -0700

    make sure we're working with strings when replacing

--- macros/clean_schedule.sql
+++ macros/clean_schedule.sql
@@ -1,3 +1,3 @@
 {% macro clean_schedule(column_name) -%}
-    replace(replace(replace(replace({{ column_name }}, '{', ''), '}', ''), '"', ''), ' ', '')
+    replace(replace(replace(replace(cast({{ column_name }} as {{ dbt.type_string() }}), '{', ''), '}', ''), '"', ''), ' ', '')
 {%- endmacro %}
