9bae1bf532420fed6f9454e51e7985ffa64c8902
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2026-08-21 17:23:54 -0500

    Update stg_zendesk__ticket_custom_field.sql

--- models/staging/stg_zendesk__ticket_custom_field.sql
+++ models/staging/stg_zendesk__ticket_custom_field.sql
@@ -11,12 +11,6 @@ with base as (
 fields as (
 
     select
-        /*
-        The below macro is used to generate the correct SQL for package staging models. It takes a list of columns
-        that are expected/needed (staging_columns from dbt_zendesk/models/tmp/) and compares it with columns
-        in the source (source_columns from dbt_zendesk/macros/).
-        For more information refer to our dbt_fivetran_utils documentation (https://github.com/fivetran/dbt_fivetran_utils.git).
-        */
         {{
             fivetran_utils.fill_staging_columns(
                 source_columns=adapter.get_columns_in_relation(ref('stg_zendesk__ticket_custom_field_tmp')),
